3586105e3fdfbc633100c13de7f4b14348309235
João V. Gomes <158607434+Volkemann@users.noreply.github.com>
2026-09-03 03:05:06 -0300

    FIXED JS - Observer section

--- script.js
+++ script.js
@@ -3,24 +3,33 @@ const navegation = document.querySelector('#topbar');
 window.addEventListener('scroll', navFixed);
 
 function navFixed() {
-    if (window.scrollY > navegation.offsetHeight + 150) {
-        navegation.classList.add('ativa');
-    } else {
-        navegation.classList.remove('ativa');
-    }
+  if (window.scrollY > navegation.offsetHeight + 150) {
+    navegation.classList.add('ativa');
+  } else {
+    navegation.classList.remove('ativa');
+  }
 };
 
-const contact = document.querySelector(".contact, .about");
+
+const sections = document.querySelectorAll(
+  ".contact, .about, .skills, .projects, .footer"
+);
 
 const observer = new IntersectionObserver((entries) => {
+
   entries.forEach((entry) => {
+
     if (entry.isIntersecting) {
-      contact.classList.add("show");
+      entry.target.classList.add("show");
     }
+
   });
+
 });
 
-observer.observe(contact);
+sections.forEach((section) => {
+  observer.observe(section);
+});
 
 
 
